e6fa465f2e0453143078dd6677e9f370ea5eeece
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-03-21 18:13:58 +0100

    Text scrolling

--- BBS.UI/ScreenBase.cs
+++ BBS.UI/ScreenBase.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.IO;
-using System.Text;
+﻿// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft BBS
+// 
+// CasaSoft BBS is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft BBS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft BBS.  
+// If not, see <http://www.gnu.org/licenses/>.
 
 namespace Casasoft.BBS.UI
 {
     public class ScreenBase : IScreen
     {
-        public virtual IScreen Show() 
+        public virtual IScreen Show()
         {
             return null;
         }
--- BBS.UI/TextScreenBase.cs
+++ BBS.UI/TextScreenBase.cs
@@ -1,38 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft BBS
+// 
+// CasaSoft BBS is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft BBS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft BBS.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.IO;
-using System.Text;
 
 namespace Casasoft.BBS.UI
 {
     public class TextScreenBase : ScreenBase
     {
-        protected string Text = string.Empty;
+        protected string[] Text;
         protected void ReadText(string name)
         {
             string assets = ConfigurationManager.AppSettings.Get("assets");
             NameValueCollection texts = (NameValueCollection)ConfigurationManager.GetSection("Texts");
             string filename = Path.Combine(assets, texts[name]);
-            Text = File.ReadAllText(filename);
+            Text = File.ReadAllLines(filename);
         }
 
         public override IScreen Show()
         {
+            Console.Clear();
             ShowText();
             return null;
         }
 
         protected void ShowText()
         {
-            ShowText(Text);
+            int currentLine = ShowLines(0,24);
+            bool inLoop = true;
+            while(inLoop)
+            {
+                ConsoleKeyInfo k = Console.ReadKey(true);
+                switch(k.KeyChar)
+                {
+                    case 'x':
+                    case 'X':
+                        inLoop = false;
+                        break;
+                    case '\r':
+                        if (Text.Length > 24 && currentLine < Text.Length - 1)
+                            ShowLines(currentLine - 23, 24);
+                        break;
+                    case ' ':
+                        if (Text.Length > 24 && currentLine < Text.Length - 1)
+                            ShowLines(currentLine, 24);
+                        break;
+                    case 'b':
+                    case 'B':
+                        if (Text.Length > 24 && currentLine > 23)
+                        {
+                            int newStart = currentLine - 48;
+                            newStart = newStart < 0 ? 0 : newStart;
+                            ShowLines(newStart, 24);
+                        }
+                        break;                    
+                }
+            }
         }
 
-        protected void ShowText(string txt)
+ 
+        protected int ShowLines(int start, int len)
         {
-            Console.Write(txt);
+            int ret = start;
+            for(;ret < start+len && ret < Text.Length; ++ret)
+            {
+                Console.WriteLine(Text[ret]);
+            }
+            return ret;
         }
-
     }
 }
