a3bf7878a234ec8219ce3d5e4cdc40c6abb7d28a
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-04-09 11:33:11 +0200

    Attributes management

--- BBS.Parser/BBSCodeListener.cs
+++ BBS.Parser/BBSCodeListener.cs
@@ -20,6 +20,8 @@
 
 using Antlr4.Runtime.Misc;
 using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
 
@@ -27,7 +29,9 @@ namespace Casasoft.BBS.Parser
 {
     public class BBSCodeListener : BBSCodeParserBaseListener
     {
-        public enum Tags { CLS, BLINK, REVERSE, BOLD, UNDERLINE, COLOR, BACKCOLOR }
+        public enum Tags { UNKNOWN, CLS, BLINK, REVERSE, BOLD, UNDERLINE, COLOR, BACKCOLOR, ACTION }
+        public static Dictionary<string, Tags> TagsTable;
+        private Stack<Tags> tagsStack;
 
         private ANSICodes.Colors currentForeColor = ANSICodes.Colors.White;
         private ANSICodes.Colors currentBackColor = ANSICodes.Colors.Black;
@@ -37,6 +41,10 @@ namespace Casasoft.BBS.Parser
         public BBSCodeListener() : base()
         {
             Parsed = string.Empty;
+            TagsTable = new Dictionary<string, Tags>();
+            foreach (Tags t in Enum.GetValues(typeof(Tags)))
+                TagsTable.Add(t.ToString().ToUpper(), t);
+
             NameValueCollection appearance = (NameValueCollection)ConfigurationManager.GetSection("Appearance");
             ANSICodes.ColorTable.TryGetValue(appearance["ForeColor"].ToUpper(), out currentForeColor);
             ANSICodes.ColorTable.TryGetValue(appearance["BackColor"].ToUpper(), out currentBackColor);
@@ -44,32 +52,110 @@ namespace Casasoft.BBS.Parser
 
         public override void EnterBbsCodeElement([NotNull] BBSCodeParser.BbsCodeElementContext context)
         {
-            string tag = context.children[1].GetText().Trim().ToUpper();
-            if (tag == Tags.CLS.ToString()) Parsed += ANSICodes.ClearScreen();
-            DisplayModeTags(tag);
-            if (tag == Tags.COLOR.ToString()) ColorTags(context.children[2]);
-            if (tag == Tags.BACKCOLOR.ToString()) ColorTags(context.children[2]);
+            string tagName = context.children[1].GetText().Trim().ToUpper();
+            Tags tag = Tags.UNKNOWN;
+            TagsTable.TryGetValue(tagName, out tag);
+            switch (tag)
+            {
+                case Tags.CLS:
+                    break;
+                case Tags.BLINK:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Blink);
+                    break;
+                case Tags.REVERSE:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Reverse);
+                    break;
+                case Tags.BOLD:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Bold);
+                    break;
+                case Tags.UNDERLINE:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Underline);
+                    break;
+                case Tags.COLOR:
+                    ColorTags(context.children[2]);
+                    break;
+                case Tags.BACKCOLOR:
+                    ColorTags(context.children[2], true);
+                    break;
+                case Tags.ACTION:
+                    break;
+                case Tags.UNKNOWN:
+                default:
+                    break;
+            }
         }
 
         public override void ExitBbsCodeElement([NotNull] BBSCodeParser.BbsCodeElementContext context)
         {
-            string tag = context.children[1].GetText().Trim().ToUpper();
-            DisplayModeTags(tag);
-            if (tag == Tags.COLOR.ToString()) Parsed += ANSICodes.SetColor(currentForeColor);
-            if (tag == Tags.BACKCOLOR.ToString()) Parsed += ANSICodes.SetBackColor(currentBackColor);
+            string tagName = context.children[1].GetText().Trim().ToUpper();
+            Tags tag = Tags.UNKNOWN;
+            TagsTable.TryGetValue(tagName, out tag);
+            switch (tag)
+            {
+                case Tags.CLS:
+                    Parsed += ANSICodes.ClearScreen();
+                    break;
+                case Tags.BLINK:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Blink);
+                    break;
+                case Tags.REVERSE:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Reverse);
+                    break;
+                case Tags.BOLD:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Bold);
+                    break;
+                case Tags.UNDERLINE:
+                    Parsed += ANSICodes.SetMode(ANSICodes.Modes.Underline);
+                    break;
+                case Tags.COLOR:
+                    Parsed += ANSICodes.SetColor(currentForeColor);
+                    break;
+                case Tags.BACKCOLOR:
+                    Parsed += ANSICodes.SetBackColor(currentBackColor);
+                    break;
+                case Tags.ACTION:
+                    break;
+                case Tags.UNKNOWN:
+                default:
+                    break;
+            }
         }
 
-        private void DisplayModeTags(string tag)
+        public override void EnterBbsCodeAttribute([NotNull] BBSCodeParser.BbsCodeAttributeContext context)
         {
-            if (tag == Tags.REVERSE.ToString()) Parsed += ANSICodes.SetMode(ANSICodes.Modes.Reverse);
-            if (tag == Tags.BLINK.ToString()) Parsed += ANSICodes.SetMode(ANSICodes.Modes.Blink);
-            if (tag == Tags.BOLD.ToString()) Parsed += ANSICodes.SetMode(ANSICodes.Modes.Bold);
-            if (tag == Tags.UNDERLINE.ToString()) Parsed += ANSICodes.SetMode(ANSICodes.Modes.Underline);
+            string tagName = context.Parent.GetChild(1).GetText().Trim().ToUpper();
+            Tags tag = Tags.UNKNOWN;
+            TagsTable.TryGetValue(tagName, out tag);
+            string attributeName = context.children[0].GetText().Trim().ToUpper();
+            string attributeValue = context.children[2].GetText().Trim('"').Trim();
+            switch (tag)
+            {
+                case Tags.CLS:
+                    if (attributeName == "FORECOLOR") ColorTags(context);
+                    if (attributeName == "BACKCOLOR") ColorTags(context, true);
+                    break;
+                case Tags.BLINK:
+                    break;
+                case Tags.REVERSE:
+                    break;
+                case Tags.BOLD:
+                    break;
+                case Tags.UNDERLINE:
+                    break;
+                case Tags.COLOR:
+                    break;
+                case Tags.BACKCOLOR:
+                    break;
+                case Tags.ACTION:
+                    break;
+                case Tags.UNKNOWN:
+                default:
+                    break;
+            }
         }
-
         private void ColorTags(IParseTree t, bool isBack = false)
         {
-            string colorName = t.GetChild(2).GetText().Trim('"').ToUpper();
+            string colorName = t.GetChild(2).GetText().Trim('"').Trim().ToUpper();
             if (isBack)
             {
                 ANSICodes.Colors color = currentBackColor;
