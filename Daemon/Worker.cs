157ea87c08d90a9a4b70322e22684293714b2787
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-04-18 16:18:20 +0200

    Worker Service

--- Daemon/Worker.cs
+++ Daemon/Worker.cs
@@ -1,3 +1,23 @@
+// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft BBS
+// 
+// CasaSoft BBS is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft BBS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft BBS.  
+// If not, see <http://www.gnu.org/licenses/>.
+
 using Casasoft.BBS.Interfaces;
 using Casasoft.BBS.Logger;
 using Casasoft.TCPServer;
@@ -47,11 +67,13 @@ namespace Casasoft.BBS.Daemon
         public override async Task StartAsync(CancellationToken cancellationToken)
         {
             ServerGlobal.Server.start();
+            EventLogger.Write("SERVER STARTED");
             await base.StartAsync(cancellationToken);
         }
 
         public override async Task StopAsync(CancellationToken cancellationToken)
         {
+            EventLogger.Write("SERVER STOPPED");
             ServerGlobal.Server.stop();
             await base.StopAsync(cancellationToken);
         }
